1809232d13d142770614770a7e49cd343fbab58a
Paul Mika Rohde <git@paulmiro.de>
2026-01-16 11:15:00 +0100

    don't use headless java to fix code-n-output builds

--- modules/java/default.nix
+++ modules/java/default.nix
@@ -7,7 +7,7 @@
 let
   cfg = config.betternix.java;
   javaToolchains = [
-    "${pkgs.jdk11_headless}/lib/openjdk"
+    "${pkgs.jdk11}/lib/openjdk"
   ];
   gradle = pkgs.gradle_8.override { inherit javaToolchains; };
 in
@@ -19,15 +19,15 @@ in
   config = lib.mkIf cfg.enable {
 
     environment.etc = {
-      "bettertec/jdk_11".source = "${pkgs.jdk11_headless}/lib/openjdk";
-      "bettertec/jdk_21".source = "${pkgs.jdk21_headless}/lib/openjdk";
+      "bettertec/jdk_11".source = "${pkgs.jdk11}/lib/openjdk";
+      "bettertec/jdk_21".source = "${pkgs.jdk21}/lib/openjdk";
       "bettertec/maven".source = "${pkgs.maven}/maven";
       "bettertec/gradle_8".source = gradle;
     };
 
     environment.systemPackages = with pkgs; [
-      jdk11_headless
-      jdk21_headless
+      jdk11
+      jdk21
       maven
       gradle
     ];
