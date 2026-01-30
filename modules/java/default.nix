0292e7a8c81b6a614d4af1a300c40a88b59979ff
Paul Mika Rohde <git@paulmiro.de>
2026-01-30 14:24:12 +0100

    rename gradle var to reflect version

--- modules/java/default.nix
+++ modules/java/default.nix
@@ -9,7 +9,7 @@ let
   javaToolchains = [
     "${pkgs.jdk11}/lib/openjdk"
   ];
-  gradle = pkgs.gradle_8.override { inherit javaToolchains; };
+  gradle_8 = pkgs.gradle_8.override { inherit javaToolchains; };
 in
 {
   options.betternix.java = {
@@ -22,14 +22,14 @@ in
       "bettertec/jdk_11".source = "${pkgs.jdk11}/lib/openjdk";
       "bettertec/jdk_21".source = "${pkgs.jdk21}/lib/openjdk";
       "bettertec/maven".source = "${pkgs.maven}/maven";
-      "bettertec/gradle_8".source = gradle;
+      "bettertec/gradle_8".source = gradle_8;
     };
 
     environment.systemPackages = with pkgs; [
       jdk11
       jdk21
       maven
-      gradle
+      gradle_8
     ];
   };
 
