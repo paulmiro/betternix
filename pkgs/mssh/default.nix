0e142147cf79fd19b11d7c19a2f0fd83e1b58096
Paul Mika Rohde <git@paulmiro.de>
2026-03-24 12:36:15 +0100

    add package: mssh

---
+++ pkgs/mssh/default.nix
@@ -0,0 +1,99 @@
+{
+  writers,
+  zellij,
+  ...
+}:
+writers.writeRubyBin "mssh" { } ''
+  require 'erb'
+  require 'tempfile'
+
+  def usage
+      puts
+      puts "Multi-SSH: run a command on multiple servers at once"
+      puts
+      puts "Usage: mssh <environment> <server_type> <numbers...> -- <command>"
+      puts "Example: mssh test web 1 2 -- tail -f /var/log/nginx/access.log"
+      exit 1
+  end
+
+  if ARGV.length < 3
+      puts "Error: Not enough arguments"
+      usage
+  end
+
+  if ARGV.index("--") == nil
+      puts "Error: No -- separator given"
+      usage
+  end
+
+  environment = ARGV.shift
+  server_type = ARGV.shift
+
+  numbers = []
+  command = nil
+  ARGV.each do |arg|
+      if arg == "--"
+          command = ARGV.drop(ARGV.index("--") + 1).join(" ")
+          break
+      end
+      numbers << arg
+  end
+
+  if environment == "dev"
+      names = numbers.map { |n| "betterdev-#{server_type}-#{n}" }
+  elsif environment == "test"
+      names = numbers.map { |n| "bettertest-#{server_type}-#{n}" }
+  elsif environment == "prod"
+      names = numbers.map { |n| "bettertec-#{server_type}-#{n}" }
+  else
+      puts "Error: Unknown environment: #{environment}"
+      usage
+  end
+
+  if ["db", "monitor", "sftp", "web", "worker", "net", "work"].include?(server_type) == false
+      puts "Error: Unknown server type: #{server_type}"
+      usage
+  end
+
+  numbers.each do |n|
+      if n.to_i.to_s != n
+          puts "Error: Not an integer: #{n}"
+          usage
+      end
+  end
+
+  if command.nil?
+      puts "Error: No command given"
+      usage
+  end
+
+  names.each do |name|
+      `ping -c 1 -W 5 #{name}`
+      if $?.exitstatus != 0
+          puts "Error: Could not ping #{name}, is your VPN connected?"
+          exit 1
+      end
+      `ssh #{name} 'true'`
+      if $?.exitstatus != 0
+          puts "Error: Could not ssh to #{name}, is your ssh config correct?"
+          exit 1
+      end
+  end
+
+
+  layout_template = File.read("${./layout.erb}")
+  pane_template = File.read("${./pane.erb}")
+
+  panes = ""
+  names.each do |name|
+      panes += ERB.new(pane_template).result(binding)
+  end
+
+  layout = ERB.new(layout_template).result(binding)
+
+  layout_file = Tempfile.new("mssh-layout.kdl")
+  layout_file.write(layout)
+  layout_file.close
+
+  exec("${zellij}/bin/zellij -l #{layout_file.path}")
+''
